e7443ba1331141136de9a1c08830c28e7b5589ef
WarriorKiller1098 <gotimenowinc123@gmail.com>
2026-06-13 08:36:54 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+const http = require("http")
+const port = 10000
+
+http.createServer(function(req, res) {
+  const urlpath = url.parse(req.url, true)
+  const parsedpath = urlpath.path
+  const query = urlpath.query
+  const path = parsedpath.split("?")[0]
+  if (path.charAt(path.length - 1) == "?") {
+      query = parsedpath.split("?")[1].replace("?", "")
+  }
+  console.log("Method requested: " + req.method + ", with endpoint " + path);
+  if (path == "/") {
+    res.write("hi!");
+    res.end();
+  } else if (path == "/status") {
+    res.write("hello I'm healthy");
+    res.end();
+  }
+}).listen(port);
