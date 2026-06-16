5e26bdd6ea296ca872ab3dc55fc1782406822b0d
WarriorKiller1098 <gotimenowinc123@gmail.com>
2026-06-16 16:04:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,32 @@ const url = require("url")
 const http = require("http")
 const port = 10000
 const fs = require("fs")
+const arls = 80
+
+function checkb(type, reqd) {
+    if (type == 1) {
+        // Assume the sender is paying to recipcent 
+        if (reqd["sc"].len() != 5) {
+            return "-5"
+        } else if (reqd["sc"].toString() == "undefined") {
+            return "-6"
+        } else if (reqd["sc"] == "") {
+            return "-6"
+        } else if (isNaN(reqd["sc"]) == true) {
+            return "-7"
+        } else if (reqd["reciever"] == "") {
+            return "-8"
+        } else if (reqd["reciever"].toString() == "undefined") {
+            return "-8"
+        } else if (isNaN(reqd["cardn"]) == true) {
+            return "-4"
+        } else if (isNaN(reqd["amount"]) == true) {
+            return "-3"
+        } else {
+            return "1"
+        }
+    }
+}
 http.createServer(function(req, res) {
     const urlpath = url.parse(req.url, true)
     const parsedpath = urlpath.path
@@ -15,34 +41,32 @@ http.createServer(function(req, res) {
         if (path == "/") {
             sendf(res, "index.html");
         } else if (path == "/status") {
-            res.write("1");
+            res.write("Good");
             res.end();
         } else if (path == "/assets/style.css") {
             sendf(res, "assets/style.css")
             res.end();
         }
     } else if (req.method == "POST") {
-        reqb = [];
-        
-        req.on("data", (ck) => {
-            reqb.push(ck);
-        });
-        
-        req.on('end', () => {
-            const cdatab = Buffer.concat(reqb).toString()
-            console.log("Processed request data body")
-            res.writeHead(200)
-            res.write(JSON.stringify({
-                status: "1",
-                data: cdatab
-            }));
-        });
-        
-        req.on('error', (err) => {
-            res.write(err);
-            res.end();
-        });
-        
+        if (path == "/payment/send") {
+            reqb = [];
+            
+            req.on("data", (ck) => {
+                reqb.push(ck);
+            });
+            
+            req.on('end', () => {
+                const cdatab = Buffer.concat(reqb).toString();
+                console.log("Processed request data body: ", cdatab);
+                res.writeHead(200);
+                res.write(checkb(1, JSON.parse(cdatab));
+            });
+            
+            req.on('error', (err) => {
+                res.write("-100 -> ", err);
+                res.end();
+            });
+        }
     }
 }).listen(port);
 
